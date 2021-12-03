393058f77b00455eb2eb75793cb476f2c8cb77c0
honestartist <skdonghyun@gmail.com>
2021-12-03 11:58:01 +0900

    제거 테스트

--- demo/src/main/java/jpa/demo/HelloController.java
+++
@@ -1,8 +0,0 @@
-@Controller
-public class HelloController {
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello");
-        return "hello1";
-    }
-}
