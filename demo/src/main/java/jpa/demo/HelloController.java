118545ebea3c77b63edc9c797486b17258ca7d79
honestartist <skdonghyun@gmail.com>
2021-12-03 11:39:59 +0900

    첫 commit

---
+++ demo/src/main/java/jpa/demo/HelloController.java
@@ -0,0 +1,8 @@
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
+        return "hello1";
+    }
+}
