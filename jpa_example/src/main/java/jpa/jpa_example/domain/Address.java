5b74cd24d4a350637288d6de310381cbe4a41334
honestartist <skdonghyun@gmail.com>
2021-12-03 13:47:08 +0900

    엔티티 설계 주의점 md 생성

--- jpa_example/src/main/java/jpa/jpa_example/domain/Address.java
+++ jpa_example/src/main/java/jpa/jpa_example/domain/Address.java
@@ -13,6 +13,8 @@ public class Address {
     private String zipcode;
 
     protected Address() {
+        //임베디드 타입( @Embeddable )은 자바 기본 생성자(default constructor)를 public 또는 protected 로 설정해야 한다.
+        //public 으로 두는 것 보다는 protected 로 설정하는 것이 그나마 더 안전하다
     }
 
     public Address(String city, String street, String zipcode) {
--- jpa_example/src/main/java/jpa/jpa_example/domain/Category.java
+++ jpa_example/src/main/java/jpa/jpa_example/domain/Category.java
@@ -35,7 +35,7 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne()
     @JoinColumn(name = "parent_id")
     private Category parent;
 
